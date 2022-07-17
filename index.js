3b79656b5017e9f659476dc4ac5feb6534f1cd2a
Cem <cemkgrbz@gmail.com>
2022-07-17 16:56:13 +0200

    modified index.js

--- index.js
+++ index.js
@@ -4,16 +4,25 @@
 
 
 function checkAge(age) {
-
-  }
+  if(age > 18) {
+    return true;
+  } return false;
+}
+console.log(checkAge(15))
 
 
 // Write a function min(a,b) which returns the least of two numbers a and b.
   function min(a, b) {
-   
+    if(a > b) {
+      return b;
+    } else if (a == b) {
+      return "Equal";
+    } return a
   }
+  console.log(min(-50,-50))
 
 // Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
   function pow(x, n) {
-    
+    return x**n;
   }
+  console.log(pow(1,100))
